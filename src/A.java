1d398b5c7475fddd3ac15e9d879c550809db0fbc
ripalpatel1451 <ripalpatel1451@gmail.com>
2020-04-23 16:11:58 -0400

    Add commit

--- src/A.java
+++ src/A.java
@@ -2,5 +2,7 @@ public class A
 {
     public static void main(String[] args) {
         System.out.println("Hello...");
+        Add add = new Add();
+        add.add(10,20);
     }
 }
---
+++ src/Add.java
@@ -0,0 +1,15 @@
+public class Add
+{
+    private int a;
+    private int b;
+    private int c;
+
+    public void add(int a, int b)
+    {
+        c = a + b;
+        System.out.println("Addition of "+a +" and " +b +" = " +c);
+
+    }
+
+}
+
