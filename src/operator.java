de6c2943229b9f98be7370c2404db3aed4c43e20
Ripal Patel <ripalp@opentext.com>
2021-01-08 13:24:56 -0500

    first commit

--- src/operator.java
+++ src/operator.java
@@ -5,6 +5,7 @@ class Operator
     public void preincrement(int i)
     {
         System.out.println("Value of (++i) i: "+ ++i);
+System.out.println("ABC");
     }
     public void postincrement(int i)
     {
