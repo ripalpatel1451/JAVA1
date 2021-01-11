14e1a07c6118a03fabda413838325d597e0d0bcb
Ripal Patel <ripalp@opentext.com>
2021-01-11 12:50:08 -0500

    dev branch changes

--- src/operator.java
+++ src/operator.java
@@ -4,11 +4,10 @@ class Operator
     private int i;
     public void preincrement(int i)
     {
-        System.out.println("Value of (++i) i: "+ ++i);
-System.out.println("ABC");
+        System.out.println("Value of (++i) i::: "+ ++i);
     }
     public void postincrement(int i)
     {
-        System.out.println("Value of (i++) i: "+ i++ +" and Value of i after postincrement" +i);
+        System.out.println("Value of (i++) i::: "+ i++ +" and Value of i after postincrement" +i);
     }
 }
